b421cbfb562d080a1e28b15fef70180da52b0977
Alin Eugen Deac <aedart@gmail.com>
2017-09-03 13:49:21 +0200

    Change helper, add additional support traits

--- src/Traits/TestHelperTrait.php
+++ src/Traits/TestHelperTrait.php
@@ -4,15 +4,18 @@ use Illuminate\Foundation\Testing\Concerns\InteractsWithAuthentication;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithConsole;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithExceptionHandling;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithSession;
 use Illuminate\Foundation\Testing\Concerns\MakesHttpRequests;
 use Illuminate\Foundation\Testing\Concerns\MocksApplicationServices;
 use Orchestra\Testbench\Traits\WithFactories;
+use Orchestra\Testbench\Traits\WithLaravelMigrations;
+use Orchestra\Testbench\Traits\WithLoadMigrationsFrom;
 
 /**
  * Trait Test Helper
  *
- * @see \Aedart\Testing\Laravel\Interfaces\ITestHelper
+ * @see \Aedart\Testing\Laravel\Contracts\TestCase
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  * @package Aedart\Testing\Laravel\Traits
@@ -24,8 +27,11 @@ trait TestHelperTrait
     use MakesHttpRequests;
     use InteractsWithAuthentication;
     use InteractsWithConsole;
+    use InteractsWithExceptionHandling;
     use InteractsWithDatabase;
     use InteractsWithSession;
     use MocksApplicationServices;
     use WithFactories;
+    use WithLaravelMigrations;
+    use WithLoadMigrationsFrom;
 }
